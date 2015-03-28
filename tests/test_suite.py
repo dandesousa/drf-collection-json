4ee050a802ae028f429d704329979ff226c26d25
Dan DeSousa <dan+github@desousa.cc>
2015-03-27 20:50:06 -0400

    adding tests

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -3,17 +3,54 @@
 
 
 import tests.conftests
+from collection_json import Collection
+from django.conf.urls import patterns, url
 from django.test import TestCase
-import unittest
+from drf_collection_json.renderers import CollectionJSONRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class InvalidClientTest(TestCase):
-    urls = "tests.urls"
+class DictionaryTest(TestCase):
+    """tests when the response contains a dictionary"""
+    urls = "tests.test_suite"
 
     def test_no_serializer_view(self):
         with self.assertRaises(TypeError):
             self.client.get("/noserializer")
 
 
-if __name__ == "__main__":
-    unittest.main()
+class ListTest(TestCase):
+    """tests when the response contains a list"""
+    urls = "tests.test_suite"
+
+
+class CollectionJSONTest(TestCase):
+    """tests when the response contains a collection+json object"""
+    urls = "tests.test_suite"
+
+    def test_no_serializer_view(self):
+        self.client.get("/noserializer/cj")
+
+
+# various views for tests
+class NoSerializerView(APIView):
+    renderer_classes = (CollectionJSONRenderer,)
+
+    def get(self, request):
+        return Response()
+
+
+class NoSerializerCjView(APIView):
+    renderer_classes = (CollectionJSONRenderer,)
+
+    def get(self, request):
+        collection = Collection(href="")
+        return Response(collection)
+
+# url patterns for tests
+urlpatterns = patterns(
+    '',
+    url(r'^noserializer$', NoSerializerView.as_view()),
+    url(r'^noserializer/cj$', NoSerializerCjView.as_view()),
+)
